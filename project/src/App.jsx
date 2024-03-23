bb9071d4b43d560af8b066ad4e8f1d7c1e49989e
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-23 11:54:39 +0545

    'router'

---
+++ project/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Details from "./pages/Details";
+import Footer from "./components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <Header />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
