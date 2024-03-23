271e70290ba2f91da78d4241e30412656f7d9afc
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-23 12:17:37 +0545

    'Header'

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink
+          to={"/"}
+          className="text-black hover:text-gray-700 duration-300"
+        >
+          Food recipe finder
+        </NavLink>
+      </h2>
+      <form>
+        <input
+          name="input"
+          placeholder="Enter items..."
+          type="text"
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        ></input>
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink
+            to={"/"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
 
-export default Header
+        <li>
+          <NavLink
+            to={"/favorites"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Favorites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
